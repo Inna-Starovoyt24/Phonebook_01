466be0d184f73d0d1fdfc17a5879d54d00ae0c49
Inna-Starovoyt24 <starovoytinna6@gmail.com>
2026-04-21 19:50:22 +0200

    Add7.2

---
+++ src/main/java/com/phonebook/details/UserDetails.java
@@ -0,0 +1,9 @@
+package com.phonebook.details;
+
+public class UserDetails {
+
+    public static final String EMAIL = "starovoytinna6@gmail.com";
+    public static final String PASSWORD = "Aa12345!";
+
+
+}
---
+++ src/test/java/com/phonebook/tests/AddContactTests.java
@@ -0,0 +1,45 @@
+package com.phonebook.tests;
+
+import com.phonebook.core.TestBase;
+import com.phonebook.data.ContactData;
+import com.phonebook.data.UserData;
+import com.phonebook.models.Contact;
+import com.phonebook.models.User;
+import com.phonebook.utils.MyDataProviders;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class AddContactTests extends TestBase {
+
+    @BeforeMethod
+    public void precondition(){
+        if (!app.getUser().isLoginLinkPresent()){
+            app.getUser().clickOnSignOutButton();
+        }
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
+                .setEmail(UserData.email)
+                .setPassword(UserData.password));
+        app.getUser().clickOnLoginButton();
+    }
+    @Test(dataProvider = "addNewContactFromCsv",dataProviderClass = MyDataProviders.class)
+    public void addContactPositiveTest(Contact contact){
+
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(contact);
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().verifyByNamePhone(contact.getPhone()));
+
+    }
+
+    @AfterMethod
+    public void postConditions(){
+        app.getContact().removeContact();
+    }
+
+
+}
